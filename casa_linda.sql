a2a4d7005b1bf0bb9cae853a072c3a9f3f1874a1
João Roberto Gil Cavina <joaorobertogilcavina@MacBook-Pro-de-Joao.local>
2022-06-03 17:09:53 -0300

    Export DB

--- casa_linda.sql
+++ casa_linda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 01/06/2022 às 19:18
+-- Tempo de geração: 03/06/2022 às 22:09
 -- Versão do servidor: 10.4.21-MariaDB
 -- Versão do PHP: 7.4.28
 
@@ -29,14 +29,25 @@ USE `casa_linda`;
 -- Estrutura para tabela `Movements`
 --
 
-CREATE TABLE `Movements` (
-  `id` int(11) NOT NULL,
-  `date` date NOT NULL,
+CREATE TABLE IF NOT EXISTS `Movements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `amount` int(11) NOT NULL,
   `price` float NOT NULL,
   `id_product` int(11) NOT NULL,
-  `type` enum('Entrada','Saída') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `type` enum('Entrada','Saída') NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_movement_id_product` (`id_product`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Despejando dados para a tabela `Movements`
+--
+
+INSERT INTO `Movements` (`id`, `date`, `amount`, `price`, `id_product`, `type`) VALUES
+(1, '2022-06-02 13:24:23', 5, 2, 8, 'Entrada'),
+(2, '2022-06-02 13:24:50', 2, 4, 8, 'Saída'),
+(3, '2022-06-02 17:01:42', 500, 50, 10, 'Saída');
 
 --
 -- Gatilhos `Movements`
@@ -56,9 +67,10 @@ DELIMITER ;
 -- Estrutura para tabela `ProductAmount`
 --
 
-CREATE TABLE `ProductAmount` (
+CREATE TABLE IF NOT EXISTS `ProductAmount` (
   `id_product` int(11) NOT NULL,
-  `amount` int(11) NOT NULL DEFAULT 0
+  `amount` int(11) NOT NULL DEFAULT 0,
+  KEY `fk_id_product` (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -68,7 +80,9 @@ CREATE TABLE `ProductAmount` (
 INSERT INTO `ProductAmount` (`id_product`, `amount`) VALUES
 (4, 0),
 (6, 0),
-(7, 0);
+(8, 3),
+(10, -500),
+(20, 0);
 
 -- --------------------------------------------------------
 
@@ -76,13 +90,14 @@ INSERT INTO `ProductAmount` (`id_product`, `amount`) VALUES
 -- Estrutura para tabela `Products`
 --
 
-CREATE TABLE `Products` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `category` varchar(20) NOT NULL,
-  `image` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `category` tinytext NOT NULL,
+  `image` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 --
 -- Despejando dados para a tabela `Products`
@@ -91,7 +106,9 @@ CREATE TABLE `Products` (
 INSERT INTO `Products` (`id`, `name`, `description`, `category`, `image`) VALUES
 (4, 'Pipoquinha', 'Pipipipipipopopopoca', 'Banheiro', NULL),
 (6, 'Bicicleta', 'Bicicleta Description', 'Sala', ''),
-(7, 'Pica de Madeira', 'Uma majestosa Pica de Madeira', 'Sala', '');
+(8, 'Copo de Plástico', 'Copo de Plástico 400ml Branco', 'Cozinha', ''),
+(10, 'Teste', 'desc', 'Cozinha', '05e37255176861d2b43ab919e3966216.jpg'),
+(20, 'mcat', 'teste', 'Cozinha, Banheiro', NULL);
 
 --
 -- Gatilhos `Products`
@@ -107,13 +124,16 @@ DELIMITER ;
 -- Estrutura para tabela `Users`
 --
 
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET utf8 NOT NULL,
   `email` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `username` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(50) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `username` varchar(20) COLLATE utf8_bin NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Despejando dados para a tabela `Users`
@@ -121,59 +141,6 @@ CREATE TABLE `Users` (
 
 INSERT INTO `Users` (`id`, `name`, `email`, `username`, `password`) VALUES
 (1, 'Administrador', 'joaoroberto.gc.01@gmail.com', 'admin', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `Movements`
---
-ALTER TABLE `Movements`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_movement_id_product` (`id_product`);
-
---
--- Índices de tabela `ProductAmount`
---
-ALTER TABLE `ProductAmount`
-  ADD KEY `fk_id_product` (`id_product`);
-
---
--- Índices de tabela `Products`
---
-ALTER TABLE `Products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `Users`
---
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `Movements`
---
-ALTER TABLE `Movements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `Products`
---
-ALTER TABLE `Products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `Users`
---
-ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restrições para tabelas despejadas
